0969f11275d3ffe4d27aeb1510e4c4da95e907e1
Clayton <cjbrady09@gmail.com>
2013-02-20 18:00:23 -0600

    added bracket

--- src/edu/drake/cs188assignd/RossActivity.java
+++ src/edu/drake/cs188assignd/RossActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -32,4 +31,4 @@ public class RossActivity extends Activity {
 		});
 	}
 
-
+}
